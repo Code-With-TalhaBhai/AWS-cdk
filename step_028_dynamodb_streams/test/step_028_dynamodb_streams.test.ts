acc0e0b883872dab9463f34098012b5a5ff4be27
Muhammad Talha Khalid <talhakhalid411@gmail.com>
2023-07-06 18:31:09 +0500

    After Eid

---
+++ step_028_dynamodb_streams/test/step_028_dynamodb_streams.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Step028DynamodbStreams from '../lib/step_028_dynamodb_streams-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step_028_dynamodb_streams-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step028DynamodbStreams.Step028DynamodbStreamsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
